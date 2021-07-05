2f8dee5bc404fd986520a0270c71e22a422e622f
tessajo <teresa-johnsen@t-online.de>
2021-07-05 22:59:18 +0200

    datenvorbereitung erweitert

--- DataPrep.py
+++ DataPrep.py
@@ -6,30 +6,19 @@ import os
 # Path ermitteln
 path = os.path.join('Archive')
 files = os.listdir(path)
-f = []
+# print(files)
 
 for i in range(0,len(files)):
-    src = os.path.join(path,files[i])
-    # Whitespace
-    f.append(files[i].replace(' ',''))
-    # print(f[i])
-    f[i]=f[i].replace('(','')
-    f[i]=f[i].replace(')','')
-    f[i]=f[i].replace("'",'')
-    f[i]=f[i].replace('-','')
-    f[i]=f[i].replace(',','')
-    f[i]=f[i].replace('J.K.','JK')
-    # print(f[i])
-    dst = os.path.join(path,f[i])
-    os.rename(src,dst)
-    book = epub.read_epub(dst)
+
+    fpath = os.path.join('Archive',files[i])
+    book = epub.read_epub(fpath)
     # print(book)
     # print(book.get_metadata('DC','identifier'))'
     # print(book.get_metadata('DC','title'))
     # print(book.get_metadata('DC','creator'))
     # print(book.get_metadata('DC','language'))
     
-    path = os.path.join('data')
+    path = os.path.join('Data','RAW')
     if os.path.exists(path):
         with open(os.path.join(path,str(i)+'.txt'),'w',encoding='utf-8') as f:
             title = book.get_metadata('DC','title')
@@ -37,9 +26,9 @@ for i in range(0,len(files)):
             meta = '<title>' + str(title[0]) + '</title>' + '<creator>' + str(creator[0]) + '</creator>'   
             f.write(meta)
 
-    # for item in book.get_items():
-    #     if item.get_type() == ebooklib.ITEM_DOCUMENT and os.path.exists(path):
-    #         # print(item.get_content().decode('utf-8'))
-    #         with open(os.path.join(path,str(i)+'.txt'),'a',encoding='utf-8') as f:
-    #             content = item.get_body_content().decode('utf-8')
-    #             f.write(content)
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT and os.path.exists(path):
+            print(item.get_content().decode('utf-8'))
+            with open(os.path.join(path,str(i)+'.txt'),'a',encoding='utf-8') as f:
+                content = item.get_body_content().decode('utf-8')
+                f.write(content)
