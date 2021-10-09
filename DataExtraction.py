8d6b09c32ed7ff4f2aa2e54bf622c1c8904ce2a2
tessajo <teresa-johnsen@t-online.de>
2021-10-09 11:44:05 +0200

    Sent Exctraction optimiert

--- DataExtraction.py
+++ DataExtraction.py
@@ -3,9 +3,8 @@ from bs4 import BeautifulSoup as bs
 from bs4.builder import HTML 
 import re
 import json
+import string
 from nltk.downloader import update
-
-from textblob_de import TextBlobDE as tb
 import nltk
 
 import DataCleanup as dc
@@ -113,10 +112,11 @@ def getSentences():
     n = {}
     names = []
     with open(os.path.join('Data','pos','names.json'),'r',encoding='utf-8') as f:
-        content = f.read().replace('\xad','')
-        n = json.loads(content)
-        for key in n.keys():
-            names.append(key)
+        content = f.read().replace('\xad','') # \xad = optional -
+    n = json.loads(content)
+    for key in n.keys():
+        names.append(key.lower())
+    '''
     adjectives = []
     with open(os.path.join('Data','pos','adjectives.json'),'r',encoding='utf-8') as f:
         content = f.read().replace('\xad','')
@@ -128,20 +128,34 @@ def getSentences():
         content = f.read().replace('\xad','')
         n = json.loads(content)
         for key in n.keys():
-            adverbs.append(key)
+            adverbs.append(key) '''
     # Alle Texte auslesen
     with open(os.path.join('Data','REFINED','master.txt'),'r',encoding='utf-8') as f:
         content = f.read().replace('\xad','') 
     sentences = nltk.sent_tokenize(content,language='english')
+    filtered_text = ''
+    text = ''
     if not os.path.exists(os.path.join('Data','RESULTS','allsentences.txt')) and not os.path.exists(os.path.join('Data','RESULTS','sentenceswithnames.txt')):
         with open(os.path.join('Data','RESULTS','sentenceswithnames.txt'),'w',encoding='utf-8') as f:
             f.write('')        
         with open(os.path.join('Data','RESULTS','allsentences.txt'),'w',encoding='utf-8') as f:
             f.write('')
-    for s in sentences:
-        res = s + '\n\n'
-        if re.search(names[0],s) and (re.search(adjectives[0],s) or re.search(adverbs[0],s)):
-            with open(os.path.join('Data','RESULTS','sentenceswithnames.txt'),'a',encoding='utf-8') as f:
-                f.write(res) 
-        with open(os.path.join('Data','RESULTS','allsentences.txt'),'a',encoding='utf-8') as f:
-            f.write(res)
+    for sentence in sentences:
+        seq = nltk.word_tokenize(sentence.lower())
+        l_seq = len(seq)-1
+        filtered_text += findWordInSentence(sentence,seq,names,l_seq)
+        text += sentence + '\n'
+    with open(os.path.join('Data','RESULTS','sentenceswithnames.txt'),'a',encoding='utf-8') as f:
+        f.write(filtered_text) 
+    with open(os.path.join('Data','RESULTS','allsentences.txt'),'a',encoding='utf-8') as f:
+        f.write(text)
+
+def findWordInSentence(sentence,seq,comparison:list,i):
+    if seq[i] in comparison:
+        return sentence
+    elif i>0:
+        return findWordInSentence(sentence,seq,comparison,i-1)
+    else:
+        return ''
+
+getSentences()
